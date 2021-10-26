cf731a79b0b8d7fbb5814373b254d924ddc1c06e
Lostinrave <83598946+Lostinrave@users.noreply.github.com>
2021-10-26 18:52:29 +0300

    Components and service

---
+++ src/app/character.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+
+// Angular dekoratorius @Injectable
+@Injectable({
+  providedIn: 'root'
+})
+export class CharacterService {
+
+  constructor() { }
+}
